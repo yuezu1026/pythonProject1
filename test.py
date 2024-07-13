2e1b0d822a7d03f28516217dad56c944263a10d2
yuezu1026 <39002818@qq.com>
2024-07-13 22:03:56 +0800

    xxxxd

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4])
-plt.ylabel('numbers')
-plt.xlabel('xxm')
-plt.legend()
-
+x=[1,2,3,4,5]
+squares=[1,4,9,16,25]
+plt.plot(x,squares,linewidth=5)
+plt.title('Number',fontsize=18,fontweight='bold')
 plt.show()
 
 
