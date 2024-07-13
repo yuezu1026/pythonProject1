3ec2b4d9b1b45b07a1bbfe6c79ec562567baccaa
yuezu1026 <39002818@qq.com>
2024-07-13 18:08:59 +0800

    addd

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("你好，我爱你")
+print("wkakwdkdk")
+
+
+a={'name':'gaoqi','age':18}
+b={'name':'gaoxixi'}
+a.update(b)
+print(a)
+
+def mytest(name,age):
+    print('my name is %s,my age is %d' %(name,age))
+
+mytest('yasaka',19)
+
+
+def mytest(name,age=18):
+    print('my name is %s,my age is %d' %(name,age))
+
+mytest('yasaka2',20)
+
+mytest(name='dskdsk',age=2)
+mytest(age=21,name='dskdsk')
+
+
+
