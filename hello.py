98d7e4fdbaffb4d9c9ea7b8dd098c061fd167eb8
yuezu1026 <39002818@qq.com>
2024-07-13 18:49:36 +0800

    addd

--- hello.py
+++ hello.py
@@ -29,5 +29,49 @@ mytest('fdsfkds',19)
 mytest('fdsfkds',19,'soccer','basketball','baseball')
 
 
+def mytest(name,age,**hobby):
+    print('my name is %s,my age is %d' %(name,age))
+    print(hobby,len(hobby))
+mytest('fdsfkds',19,first_hobby='basketball')
+
+
+##递归
+data = list(range(1,101))
+print(data)
+# sum = 0
+# for x in data:
+#     sum  += x
+# sum
+#
+# print(sum)
+
+
+def my_add(a,b):
+    if len(a) == 2:
+        return a[0] + a[1] + b
+    return my_add(a[:-1],a[-1]) + b
+
+a = my_add(data[:-1],data[-1])
+
+print(a)
+
+
+myseq=[128, 45.67, -6.2e8]
+def mytest(num):
+    return num * 2
+print(mytest(10))
+print(mytest(myseq))
+
+myseq = [123,45.67,-6.2e8]
+
+def convert(func,seq):
+    print('转换序列中的数值,要他们同意为一样的数据类型')
+    return [func(eachNum) for eachNum in seq]
+
+print(convert(int,myseq))
+
+
+
+
 
 
